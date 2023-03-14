🐛 Fix clip model vector normalization bug

clip_server/clip_model.py
@@ -1,6 +1,6 @@
 import torch
+from torch.nn.functional import normalize
 import clip
-from sklearn.preprocessing import normalize
 import numpy as np
 
 
@@ -18,9 +18,7 @@ class CLIP:
         """
         Normalizes a tensor.
         """
-        normed_feature = torch.Tensor(
-                normalize(feature.detach().cpu().reshape(1, -1)).flatten().tolist()
-            )
+        normed_feature = normalize(feature, p=2, dim=1)
         return normed_feature
 
     

preprocess/extract_clip_features.py
@@ -10,7 +10,7 @@ import os
 import glob
 
 
-BATCH_INDEX_FOR_SAVING = 500
+BATCH_INDEX_FOR_SAVING = 1
 DEFAULT_DATASET_NAME = 'lsc23'
 DEFAULT_DEVICE = 'cuda'
 DEFAULT_MODEL_NAME = 'ViT-L/14'
@@ -102,11 +102,11 @@ def post_processing(features_folder_path: str, dataset_name: str):
 
     for file in glob.glob(f'{features_folder_path}/{dataset_name}_features_*.pt'):
         corresponding_indices_file = file.replace('features', 'indices')
-        features.append(torch.load(file))
-        feature_indices.append(torch.load(corresponding_indices_file))
+        feats = torch.load(file)
+        features.append(feats)
+        feature_indices += torch.load(corresponding_indices_file)
     
     features = torch.cat(features, dim=0)
-    feature_indices = torch.cat(feature_indices, dim=0)
 
     # Save the final features and indices
     model_name = DEFAULT_MODEL_NAME.replace('/', '-')
@@ -163,4 +163,6 @@ if __name__ == '__main__':
     create_folder(args.output_folder_path)
     
     run(args)
+
+    print('Post-processing...')
     post_processing(args.output_folder_path, args.dataset_name)